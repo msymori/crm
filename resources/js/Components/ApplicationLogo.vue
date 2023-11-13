08090eadd0af02b0c839294772abb4202fdc0f00
Masaya Mori <41807947+msymori@users.noreply.github.com>
2023-11-13 21:37:12 +0900

    analysis

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,23 @@
+<script setup>
+import { Chart, registerables } from "chart.js";
+import { BarChart } from "vue-chart-3";
+import { reactive } from "vue"
+
+Chart.register(...registerables);
+
+const barData = reactive({
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  datasets: [
+    {
+      label: '売上',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    }
+  ]
+})
+</script>
+
 <template>
     <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg">
         <path
